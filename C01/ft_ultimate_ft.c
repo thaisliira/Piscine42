94c9f842bee65ec7321d10cbc2901d67b347f3b4
Thais Lira <157423682+thaisliira@users.noreply.github.com>
2024-10-21 16:08:54 +0100

    Add files via upload

---
+++ C01/ft_ultimate_ft.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlira-ri <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/08 15:24:47 by tlira-ri          #+#    #+#             */
+/*   Updated: 2024/09/08 17:09:13 by tlira-ri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+/*int	main(void)
+{
+	int	n;
+	int	*nbr;
+	int	**nbr2;
+	int	***nbr3;
+	int	****nbr4;
+	int	*****nbr5;
+	int	******nbr6;
+	int	*******nbr7;
+	int	********nbr8;
+	int	*********nbr9;
+
+	n = 10;
+	nbr = &n;
+	nbr2 = &nbr;
+	nbr3 = &nbr2;
+	nbr4 = &nbr3;
+	nbr5 = &nbr4;
+	nbr6 = &nbr5;
+	nbr7 = &nbr6;
+	nbr8 = &nbr7;
+	nbr9 = &nbr8;
+	printf("%d\n", n);
+	ft_ultimate_ft(nbr9);
+	printf("%d", n);
+}*/
